31f19fe108690fcc950d8d16725b3e185bec564c
mostafaanasr <mostafaanasrr533@gmail.com>
2023-03-16 18:25:05 -0700

    C-Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Description: program that print a statement with (printf)
+ *
+ * Return: alwayz zero
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
