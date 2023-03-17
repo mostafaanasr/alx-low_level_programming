723b6959b293fef893d012a20fd64dfb2a0213dd
mostafaanasr <mostafaanasrr533@gmail.com>
2023-03-16 18:43:49 -0700

    C-Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * Main - entry point
+ * Main - Entry point
  *
  * Description: program that prints the size of int, char, double
  *
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	printf("size of a char : %ld\n", sizeof(char));
-	printf("size of an int : %ld\n", sizeof(int));
-	printf("size of a long int : %ld\n", sizeof(long int));
-	printf("size of a long long int : %ld\n", sizeof(long long int));
-	printf("size of a float : %ld\n", sizeof(float));
+	printf("size of a char : %ld byte(s)\n", sizeof(char));
+	printf("size of an int : %ld byte(s)\n", sizeof(int));
+	printf("size of a long int : %ld byte(s)\n", sizeof(long int));
+	printf("size of a long long int : %ld byte(s)\n", sizeof(long long int));
+	printf("size of a float : %ld byte(s)\n", sizeof(float));
 	return (0);
 }
